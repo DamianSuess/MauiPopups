e31472284c76a4f5b3a7cb0704626784b55ca827
Damian Suess <djsuess@gmail.com>
2024-08-17 15:22:55 -0400

    base app

---
+++ SamplePopup/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SamplePopup
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
